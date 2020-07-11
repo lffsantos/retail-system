a8f2ae5ede05edfa2be29ce68068e7024443293f
Lucas <lffsantos@gmail.com>
2020-07-10 21:07:25 -0300

    initial commit, create database and populate , using docker on postgres

---
+++ README.md
@@ -0,0 +1,4 @@
+- rodar o docker para subir o postgres 
+- criar o banco de dados
+- popular o banco de dados com dados fake
+
---
+++ create_database.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import psycopg2
+
+
+def create_tables():
+    """ create tables in the PostgreSQL database"""
+    commands = (
+        """
+        CREATE TABLE users (
+            id SERIAL PRIMARY KEY,
+            first_name VARCHAR(100) NOT NULL,
+            last_name VARCHAR(100) NOT NULL,
+            birth_date DATE NOT NULL
+        )
+        """,
+        """ CREATE TABLE products (
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(150) NOT NULL,
+                description VARCHAR(255),
+                price NUMERIC (5, 2) NOT NULL,
+                base_discount_percent FLOAT NOT NULL DEFAULT 0
+                )
+        """,
+        )
+    conn = None
+    try:
+        conn = psycopg2.connect(dbname="varejo", user="docker", password="docker", host='127.0.0.1', port='5431')
+        cur = conn.cursor()
+        # create table one by one
+        for command in commands:
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
+        cur.close()
+        # commit the changes
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+if __name__ == '__main__':
+    create_tables()
