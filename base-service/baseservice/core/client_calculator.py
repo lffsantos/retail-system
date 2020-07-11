488f92216741ea94be6fa2ebca2a68067dc1bcf7
Lucas <lffsantos@gmail.com>
2020-07-11 01:03:16 -0300

    check timeout connect

--- base-service/baseservice/core/client_calculator.py
+++ base-service/baseservice/core/client_calculator.py
@@ -8,14 +8,15 @@ def get_discount(product_id, user_id):
     filter_user = f"?userId={user_id}" if user_id else ""
     url = f"{CALCULATOR_URL}/discounts/{product_id}/{filter_user}"
     try:
-        r = requests.get(url)
+        r = requests.get(url, timeout=5)
         r.raise_for_status()
     except requests.exceptions.ConnectionError as error:
-        raise ConnectionError
+        raise error
     except requests.exceptions.HTTPError as error:
-        if error.response.status_code == 404:
+        if error.response.status_code == 404 or error.response.status_code == 500:
             return None
 
+    except Exception as error:
         raise error
 
     return r.json()
@@ -24,7 +25,6 @@ def get_discount(product_id, user_id):
 if __name__ == "__main__":
     try:
         discount = get_discount(1, 1)
-        print(discount)
     except Exception as error:
         if isinstance(error, ConnectionError):
             print("Connection Refused")
